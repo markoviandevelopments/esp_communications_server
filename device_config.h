9fae26e899373107adb4813c44f5f3b7795169e8
Willoh Brubaker <167579833+willohbrubaker@users.noreply.github.com>
2025-02-07 15:07:52 -0600

    Create device_config.h

---
+++ device_config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Uncomment ONLY ONE per device
+// #define IS_LED_STRIP
+#define IS_MATRIX
+
+// Shared WiFi config
+const char* ssid = "Brubaker Wifi";
+const char* password = "Pre$ton01";
