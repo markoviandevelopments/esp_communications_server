1c6ba0fb5aa59c76d3c8b028bafd98ef7ea5f1f9
Preston Brubaker <brubakerpreston@gmail.com>
2025-02-08 23:25:07 -0600

    cleanup!!

--- device_config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Uncomment ONLY ONE per device
-// #define IS_LED_STRIP
-#define IS_MATRIX
-
-// Shared WiFi config
-const char* ssid = "Brubaker Wifi";
-const char* password = "Pre$ton01";
